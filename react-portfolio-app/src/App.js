245a3cf018479cb43074a7265e09cc555f774abf
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-20 13:57:11 -0400

    connecting routes through react-router-dom

--- react-portfolio-app/src/App.js
+++ react-portfolio-app/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './index.css';
-import './routes/Home';
-import './routes/About';
-import './routes/Project';
-import './routes/Contact';
+
+import {Route, Routes} from "react-router-dom";
+
+import Home from'./routes/Home';
+import About from'./routes/About';
+import Project from'./routes/Project';
+import Contact from './routes/Contact';
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/project' element={<Project />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
     </>
   );
 }
--- react-portfolio-app/src/routes/Project.js
+++ react-portfolio-app/src/routes/Project.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Project = () => {
-  return <div>Project</div>;
+  return <div>I'm projecting right now</div>;
 };
 
 export default Project;

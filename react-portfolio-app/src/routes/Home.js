4bac772dae1dffc4a9b45c4e20534f1eadbfc408
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-20 14:13:40 -0400

    adding component folder/file structure - creating Navigation bar

--- react-portfolio-app/src/routes/Home.js
+++ react-portfolio-app/src/routes/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
-  return <div>This is my house!</div>;
+  return <div>
+    <Navbar/>
+  </div>;
 };
 
 export default Home;

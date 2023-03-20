56f4ec6182efd2f1aada8468025d924ccc6d7459
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-20 18:54:59 -0400

    hamburger menu nav functionality

--- react-portfolio-app/src/components/Navbar.js
+++ react-portfolio-app/src/components/Navbar.js
@@ -1,17 +1,20 @@
 import './NavbarStyles.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <div className="header">
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -25,8 +28,12 @@ const Navbar = () => {
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
-      <div className="hamburger">
-        <FaBars size={20} style={{ color: 'white' }} />
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: 'white' }} />
+        ) : (
+          <FaBars size={20} style={{ color: 'white' }} />
+        )}
       </div>
     </div>
   );

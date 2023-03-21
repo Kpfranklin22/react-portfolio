c117b1007f2ec53aca6cc64cacc16582c9e6769c
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-21 12:53:46 -0400

    adding img components to all route files - editing headings and text - styling of heroimg2

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="ABOUT." text="I am a Coder. I code things. You see this? I coded it."/>
       <Footer />
     </div>
   );

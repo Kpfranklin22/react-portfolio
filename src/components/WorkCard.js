95267abbcefddf5e0d5ede4e96d9939811e2cb4d
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-21 13:34:37 -0400

    editing file names - adding component workcardjs/css for projects

---
+++ src/components/WorkCard.js
@@ -0,0 +1,19 @@
+import './WordCardStyles.css';
+
+// import React from 'react';
+// import project1 from "../assets/"
+
+const WorkCard = () => {
+  return (
+    <div className="work-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card">
+          <img src="" alt=""></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkCard;

7c3030fafb9e1853fc7b9fc50510f162402e27df
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-21 14:44:48 -0400

    linking my 1st project, styling the workcardcss

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,7 +1,8 @@
 import './WordCardStyles.css';
 
 import React from 'react';
-// import project1 from "../assets/"
+import projectPokedex from '../assets/project-pokedex.png';
+import { NavLink } from 'react-router-dom';
 
 const WorkCard = () => {
   return (
@@ -9,7 +10,25 @@ const WorkCard = () => {
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
         <div className="project-card">
-          <img src="" alt=""></img>
+          <img src={projectPokedex} alt="image" />
+          <h2 className="project-title">Project Title</h2>
+          <div className="project-details">
+            <p>This is some text</p>
+            <div className="project-btns">
+            <NavLink
+              to="https://kpfranklin22.github.io/project-pokedex/"
+              className="btn"
+            >
+              View
+            </NavLink>
+            <NavLink
+              to="https://github.com/Kpfranklin22/project-pokedex"
+              className="btn"
+            >
+              Source
+            </NavLink>
+            </div>
+          </div>
         </div>
       </div>
     </div>
